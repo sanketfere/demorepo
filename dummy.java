b8420037665dc846f5ec9631ff83f9193f6a4217
Sanket H Fere <154800998+sanketfere@users.noreply.github.com>
2026-03-02 14:16:42 +0530

    Update dummy.java

--- dummy.java
+++ dummy.java
@@ -2,7 +2,7 @@ public class dummy{
     public static void main(String[] args){
         System.out.println("pushing this file to github");
         System.out.println("Adding another line to this code");
-        System.out.println("Addition " + (2+3));
+        System.out.println("Addition " + (2+30));
         System.out.println("Adding another line");
     }
 }
